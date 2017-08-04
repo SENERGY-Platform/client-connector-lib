60ddead677ca174f1ae78abd6f802e97962673f9
y-du <42994541+y-du@users.noreply.github.com>
2017-08-04 15:01:31 +0200

    initial commit

---
+++ modules/singleton.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    exit('Please use "client.py"')
+
+try:
+    from modules.logger import root_logger
+except ImportError as ex:
+    exit("{} - {}".format(__name__, ex.msg))
+
+logger = root_logger.getChild(__name__)
+
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
