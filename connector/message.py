f23a4caa2413c49544481cf46a1f2e3725770d21
y-du <42994541+y-du@users.noreply.github.com>
2017-08-04 15:03:07 +0200

    removed old parts

--- connector/message.py
+++ connector/message.py
@@ -13,23 +13,11 @@ logger = root_logger.getChild(__name__)
 class Message:
     _device_id_key = 'device_url'
     _endpoint_key = 'service_url'
-    _endpoint_name_key = 'service_name'
-    _timestamp_key = 'time'
-    _task_id_key = 'task_id'
     _payload_key = 'protocol_parts'
-    _instance_id_key = 'device_instance_id'
-    _worker_id_key = 'worker_id'
-    _output_name_key = 'output_name'
 
     def __init__(self):
         self._device_id = str()       # device_url
         self._endpoint = str()        # service_url (sepl)
-        self._endpoint_name = str()   # service_name (sepl)
-        self._timestamp = int()       # time (sepl)
-        self._task_id = str()         # task_id (sepl)
-        self._instance_id = str()     # device_instance_id (sepl)
-        self._worker_id = str()       # worker_id (sepl)
-        self._output_name = str()     # output_name (sepl)
         self._payload_header = str()  # protocol_parts
         self._payload = str()         # protocol_parts
 
@@ -107,13 +95,7 @@ class Message:
         msg_struct = {
             Message._device_id_key: message._device_id,
             Message._endpoint_key: message._endpoint,
-            Message._endpoint_name_key: message._endpoint_name,
-            Message._timestamp_key: str(message._timestamp),
-            Message._task_id_key: message._task_id,
             Message._payload_key: payload,
-            Message._instance_id_key: message._instance_id,
-            Message._worker_id_key: message._worker_id,
-            Message._output_name_key: message._output_name
         }
         msg_str = json.dumps(msg_struct)
         return msg_str
@@ -125,12 +107,6 @@ class Message:
             message = json.loads(message)
             msg_obj._device_id = message.get(Message._device_id_key)
             msg_obj._endpoint = message.get(Message._endpoint_key)
-            msg_obj._endpoint_name = message.get(Message._endpoint_name_key)
-            msg_obj._timestamp = int(message.get(Message._timestamp_key))
-            msg_obj._task_id = message.get(Message._task_id_key)
-            msg_obj._instance_id = message.get(Message._instance_id_key)
-            msg_obj._worker_id = message.get(Message._worker_id_key)
-            msg_obj._output_name = message.get(Message._output_name_key)
             payload = message.get(Message._payload_key)
             for item in payload:
                 part = item.get('name')
