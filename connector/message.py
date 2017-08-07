55f5cb6e27cfb1f65d6d97c9b452f411f653a7e5
y-du <42994541+y-du@users.noreply.github.com>
2017-08-07 17:16:52 +0200

    refactor

--- connector/message.py
+++ connector/message.py
@@ -6,34 +6,14 @@ try:
 except ImportError as ex:
     exit("{} - {}".format(__name__, ex.msg))
 import json
-from uuid import uuid4 as uuid
 
 
 logger = root_logger.getChild(__name__)
 
-class Envelope:
-    def __init__(self, handler, message, token):
-        self.handler = handler
-        self.token = token
-        if type(message) is not Message:
-            raise TypeError("message must be of type 'Message' but got '{}'".format(type(message)))
-        self.message = message
 
-    @staticmethod
-    def open(string: str):
-        handler_and_token, message = string.split(':', maxsplit=1)
-        #### temp ####
-        if '.' in handler_and_token:
-            handler, token = handler_and_token.split('.', maxsplit=1)
-            return __class__(handler, Message.deserialize(message), token)
-        else:
-            handler = handler_and_token
-            return __class__(handler, Message.deserialize(message), uuid())
-        #### temp ####
-
-    @staticmethod
-    def close(envelope):
-        return '{}.{}:{}'.format(envelope.handler, envelope.token, Message.serialize(envelope.message))
+_device_id_key = 'device_url'
+_service_key = 'service_url'
+_protocol_parts_key = 'protocol_parts'
 
 
 class Payload:
@@ -63,17 +43,13 @@ class Payload:
 
 
 class Message:
-    _device_id_key = 'device_url'
-    _service_key = 'service_url'
-    _protocol_parts_key = 'protocol_parts'
-
     def __init__(self, device_id, payload=None, endpoint=None):
-        self.__device_id = device_id             # device_url
-        self.__endpoint = endpoint               # service_url (sepl)
         if type(payload) is not Payload:
             raise TypeError("payload must be of type 'Payload' but got '{}'".format(type(payload)))
-        self.__payload = payload or Payload()                 # protocol_parts
-        self.__token = 123
+        self.__device_id = device_id             # device_url
+        self.__endpoint = endpoint               # service_url (sepl)
+        self.__payload = payload or Payload()    # protocol_parts
+        self.__token = None
         self.__overhead = None
 
     @property
@@ -102,49 +78,49 @@ class Message:
     def endpoint(self, arg):
         raise TypeError('attribute endpoint is immutable')
 
-    @staticmethod
-    def serialize(message: __class__):
-        if type(message) is not Message:
-            raise TypeError("message must be of type 'Message' but got '{}'".format(type(message)))
-        protocol_parts = (
-            {
-                'name': 'header',
-                'value': message.payload.header
-            },
-            {
-                'name': 'body',
-                'value': message.payload.body
-            }
-        )
-        msg_struct = {
-            Message._device_id_key: message.device_id,
-            Message._service_key: message.endpoint,
-            Message._protocol_parts_key: protocol_parts,
-        }
-        ### temp ###
-        msg_struct.update(message.__overhead)
-        ### temp ###
-        return json.dumps(msg_struct)
 
-    @staticmethod
-    def deserialize(message) -> __class__:
-        try:
-            message = json.loads(message)
-        except Exception as ex:
-            logger.error(ex)
-        payload = Payload()
-        protocol_parts = message.get(Message._protocol_parts_key)
-        for part in protocol_parts:
-            name = part.get('name')
-            if name == 'body':
-                payload.body = part.get('value')
-            elif name == 'header':
-                payload.header = part.get('value')
-        msg_obj = __class__(message.get(Message._device_id_key), payload, message.get(Message._service_key))
-        #### temp ####
-        del message[Message._protocol_parts_key]
-        del message[Message._device_id_key]
-        del message[Message._service_key]
-        msg_obj.__overhead = message
-        #### temp ####
-        return msg_obj
+def serializeMessage(message: Message):
+    if type(message) is not Message:
+        raise TypeError("message must be of type 'Message' but got '{}'".format(type(message)))
+    protocol_parts = (
+        {
+            'name': 'header',
+            'value': message.payload.header
+        },
+        {
+            'name': 'body',
+            'value': message.payload.body
+        }
+    )
+    msg_struct = {
+        _device_id_key: message.device_id,
+        _service_key: message.endpoint,
+        _protocol_parts_key: protocol_parts,
+    }
+    ### temp ###
+    msg_struct.update(message._Message__overhead)
+    ### temp ###
+    return json.dumps(msg_struct)
+
+
+def deserializeMessage(message) -> Message:
+    try:
+        message = json.loads(message)
+    except Exception as ex:
+        logger.error(ex)
+    payload = Payload()
+    protocol_parts = message.get(_protocol_parts_key)
+    for part in protocol_parts:
+        name = part.get('name')
+        if name == 'body':
+            payload.body = part.get('value')
+        elif name == 'header':
+            payload.header = part.get('value')
+    msg_obj = Message(message.get(_device_id_key), payload, message.get(_service_key))
+    #### temp ####
+    del message[_protocol_parts_key]
+    del message[_device_id_key]
+    del message[_service_key]
+    msg_obj._Message__overhead = message
+    #### temp ####
+    return msg_obj
