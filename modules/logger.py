e98298b585f7fe10fe2a9d53790c0b984db5f678
y-du <42994541+y-du@users.noreply.github.com>
2017-08-09 10:54:48 +0200

    added rotating log

--- modules/logger.py
+++ modules/logger.py
@@ -1,12 +1,15 @@
 if __name__ == '__main__':
     exit('Please use "client.py"')
 
+from logging.handlers import TimedRotatingFileHandler
 import logging, os
 
 
-LOGLEVEL = os.getenv('LOGGING_LEVEL', 'debug')
+LOGLEVEL = os.getenv('LOGGING_LEVEL', 'info')
+LOCAL_ROTATING_LOG = os.getenv('LOCAL_ROTATING_LOG', None)
+ROTATING_LOG_BACKUP_COUNT = os.getenv('ROTATING_LOG_BACKUP_COUNT', 7)
 
-# logging level mapper
+
 logging_levels = {
     'info': logging.INFO,
     'warning': logging.WARNING,
@@ -15,12 +18,24 @@ logging_levels = {
     'debug': logging.DEBUG
 }
 
-# format log output
-logging.basicConfig(
-    #format='%(asctime)s - %(threadName)s - %(levelname)s: [%(name)s] %(message)s',
-    format='%(asctime)s - %(levelname)s: [%(name)s] %(message)s',
-    datefmt='%m.%d.%Y %I:%M:%S %p',
-    level=logging_levels[LOGLEVEL]
-)
+
+filename = os.path.join(os.path.dirname(__file__), '../connector-client.log')
+rotating_log_handler = TimedRotatingFileHandler(filename, when="midnight", backupCount=ROTATING_LOG_BACKUP_COUNT)
+
+
+if LOCAL_ROTATING_LOG:
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s: [%(name)s] %(message)s',
+        datefmt='%m.%d.%Y %I:%M:%S %p',
+        level=logging_levels[LOGLEVEL],
+        handlers=[rotating_log_handler]
+    )
+else:
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s: [%(name)s] %(message)s',
+        datefmt='%m.%d.%Y %I:%M:%S %p',
+        level=logging_levels[LOGLEVEL]
+    )
+
 
 root_logger = logging.getLogger()
