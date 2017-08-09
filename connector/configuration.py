5d7ddd5ac2671a20545185ec57d577ccd4e354d5
y-du <42994541+y-du@users.noreply.github.com>
2017-08-09 14:25:43 +0200

    added config file and config parser

--- connector/configuration.py
+++ connector/configuration.py
@@ -1,17 +1,22 @@
 if __name__ == '__main__':
     exit('Please use "client.py"')
 
-import os
+import os, configparser
 
+switch = {
+    'yes': True,
+    'no': False
+}
 
-CONNECTOR_PROTOCOL = os.getenv('CONNECTOR_HTTPS', 'ws')
-CONNECTOR_HOST = os.getenv('CONNECTOR_HOST', 'fgseitsrancher.wifa.intern.uni-leipzig.de')
-CONNECTOR_PORT = os.getenv('CONNECTOR_PORT', 8092)
-CONNECTOR_USER = ''
-CONNECTOR_PASSWORD = ''
+config = configparser.ConfigParser()
+config.read('{}/connector.conf'.format(os.getcwd()))
 
-LOGLEVEL = os.getenv('LOGGING_LEVEL', 'info')
-LOCAL_ROTATING_LOG = os.getenv('LOCAL_ROTATING_LOG', None)
-ROTATING_LOG_BACKUP_COUNT = os.getenv('ROTATING_LOG_BACKUP_COUNT', 7)
+CONNECTOR_PROTOCOL = os.getenv('CONNECTOR_PROTOCOL', config['CONNECTOR']['protocol'])
+CONNECTOR_HOST = os.getenv('CONNECTOR_HOST', config['CONNECTOR']['host'])
+CONNECTOR_PORT = os.getenv('CONNECTOR_PORT', config['CONNECTOR']['port'])
+CONNECTOR_USER = config['CONNECTOR']['user']
+CONNECTOR_PASSWORD = config['CONNECTOR']['password']
 
-# add local config parser
+LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', config['LOGGER']['level'])
+LOCAL_ROTATING_LOG = os.getenv('LOCAL_ROTATING_LOG', switch.get(config['LOGGER']['rotating_log']))
+ROTATING_LOG_BACKUP_COUNT = os.getenv('ROTATING_LOG_BACKUP_COUNT', config['LOGGER']['rotating_log_backup_count'])
