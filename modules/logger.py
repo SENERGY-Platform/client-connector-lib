4d82bfe481a8b10a4413cd16a4e16e28d8b98644
y-du <42994541+y-du@users.noreply.github.com>
2017-08-09 11:21:42 +0200

    rotating log saves to logs folder

--- modules/logger.py
+++ modules/logger.py
@@ -1,13 +1,12 @@
 if __name__ == '__main__':
     exit('Please use "client.py"')
 
+try:
+    from connector.configuration import LOGLEVEL, LOCAL_ROTATING_LOG, ROTATING_LOG_BACKUP_COUNT
+except ImportError as ex:
+    exit("{} - {}".format(__name__, ex.msg))
 from logging.handlers import TimedRotatingFileHandler
 import logging, os
-
-
-LOGLEVEL = os.getenv('LOGGING_LEVEL', 'info')
-LOCAL_ROTATING_LOG = os.getenv('LOCAL_ROTATING_LOG', None)
-ROTATING_LOG_BACKUP_COUNT = os.getenv('ROTATING_LOG_BACKUP_COUNT', 7)
 
 
 logging_levels = {
@@ -19,20 +18,23 @@ logging_levels = {
 }
 
 
-filename = os.path.join(os.path.dirname(__file__), '../connector-client.log')
+if not os.path.exists('{}/logs'.format(os.getcwd())):
+    os.makedirs('{}/logs'.format(os.getcwd()))
+filename = os.path.join(os.path.dirname(__file__), '{}/logs/connector-client.log'.format(os.getcwd()))
 rotating_log_handler = TimedRotatingFileHandler(filename, when="midnight", backupCount=ROTATING_LOG_BACKUP_COUNT)
 
 
 if LOCAL_ROTATING_LOG:
+
     logging.basicConfig(
-        format='%(asctime)s - %(levelname)s: [%(name)s] %(message)s',
+        format='%(asctime)s - %(levelname)s: [%(module)s] %(message)s',
         datefmt='%m.%d.%Y %I:%M:%S %p',
         level=logging_levels[LOGLEVEL],
         handlers=[rotating_log_handler]
     )
 else:
     logging.basicConfig(
-        format='%(asctime)s - %(levelname)s: [%(name)s] %(message)s',
+        format='%(asctime)s - %(levelname)s: [%(module)s] %(message)s',
         datefmt='%m.%d.%Y %I:%M:%S %p',
         level=logging_levels[LOGLEVEL]
     )
