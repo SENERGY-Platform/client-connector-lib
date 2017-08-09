434a41568e5e6efe036152c5bf4ceb2e5f6c7960
y-du <42994541+y-du@users.noreply.github.com>
2017-08-09 14:32:25 +0200

    used getboolean instead of switch

--- connector/configuration.py
+++ connector/configuration.py
@@ -3,20 +3,16 @@ if __name__ == '__main__':
 
 import os, configparser
 
-switch = {
-    'yes': True,
-    'no': False
-}
 
 config = configparser.ConfigParser()
 config.read('{}/connector.conf'.format(os.getcwd()))
+
 
 CONNECTOR_PROTOCOL = os.getenv('CONNECTOR_PROTOCOL', config['CONNECTOR']['protocol'])
 CONNECTOR_HOST = os.getenv('CONNECTOR_HOST', config['CONNECTOR']['host'])
 CONNECTOR_PORT = os.getenv('CONNECTOR_PORT', config['CONNECTOR']['port'])
 CONNECTOR_USER = config['CONNECTOR']['user']
 CONNECTOR_PASSWORD = config['CONNECTOR']['password']
-
 LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', config['LOGGER']['level'])
-LOCAL_ROTATING_LOG = os.getenv('LOCAL_ROTATING_LOG', switch.get(config['LOGGER']['rotating_log']))
+LOCAL_ROTATING_LOG = os.getenv('LOCAL_ROTATING_LOG', config['LOGGER'].getboolean('rotating_log'))
 ROTATING_LOG_BACKUP_COUNT = os.getenv('ROTATING_LOG_BACKUP_COUNT', config['LOGGER']['rotating_log_backup_count'])
