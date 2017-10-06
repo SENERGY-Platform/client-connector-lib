435f39bac38aa43a621ade43c4fb4050d2928fa6
y-du <42994541+y-du@users.noreply.github.com>
2017-10-06 15:39:20 +0200

    added documentation

--- connector/dm_interface.py
+++ connector/dm_interface.py
@@ -1,10 +1,6 @@
 if __name__ == '__main__':
     exit('Please use "service.py"')
 
-try:
-    from connector.device import Device
-except ImportError as ex:
-    exit("{} - {}".format(__name__, ex.msg))
 from abc import ABCMeta, abstractmethod
 
 
@@ -14,21 +10,42 @@ class DeviceManagerInterface(metaclass=ABCMeta):
     """
     @abstractmethod
     def add(self, device):
+        """
+        add device
+        :param device: takes a Device (or subclass of Device) object.
+        """
         pass
 
     @abstractmethod
     def update(self, device):
+        """
+        update device
+        :param device: takes a Device (or subclass of Device) object.
+        """
         pass
 
     @abstractmethod
-    def remove(self, d_id):
+    def remove(self, id_str):
+        """
+        add device
+        :param id_str: takes device ID as string.
+        """
         pass
 
     @abstractmethod
-    def get(self, id_str) -> Device:
+    def get(self, id_str):
+        """
+        get device
+        :param id_str: takes device ID as string.
+        :return a Device (or subclass of Device) object.
+        """
         pass
 
     @property
     @abstractmethod
-    def devices(self) -> dict:
+    def devices(self):
+        """
+        all devices
+        :return a dict, list or tuple object.
+        """
         pass
