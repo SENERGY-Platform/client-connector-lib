a58aa2b4840a4c473f297609d666ef22a922af2b
y-du <42994541+y-du@users.noreply.github.com>
2017-10-06 13:32:59 +0200

    refactor, implemented update, mute, put, remove, commit

--- connector/client.py
+++ connector/client.py
@@ -16,13 +16,11 @@ except ImportError as ex:
 import functools, json, time, hashlib
 from queue import Queue
 from threading import Thread, Event
+from inspect import isclass
 
 logger = root_logger.getChild(__name__)
 
 logger.info(20 * '*' + ' Starting SEPL connector-client ' + 20 * '*')
-
-OUT_QUEUE = Queue()
-IN_QUEUE = Queue()
 
 
 def _callback(event, message=None):
@@ -43,6 +41,33 @@ def _callInThread(function):
     thread.start()
 
 
+def _interfaceCheck(cls, interface):
+    if issubclass(cls, interface):
+        return True
+    raise TypeError("provided class '{}' must be a subclass of '{}'".format(cls, interface))
+
+
+def _synchroniseGid(remote_gid):
+    if not CONNECTOR_GID == remote_gid:
+        logger.debug('local and remote gateway ID differ: {} - {}'.format(CONNECTOR_GID, remote_gid))
+        global CONNECTOR_GID
+        CONNECTOR_GID = remote_gid
+        writeConf(section='CONNECTOR', parameter='gid', value=remote_gid)
+        logger.info("set gateway ID: '{}'".format(remote_gid))
+    else:
+        logger.debug('local and remote gateway ID match')
+
+
+def _hashDevices(devices) -> str:
+    if type(devices) not in (dict, list, tuple):
+        raise TypeError("please provide devices via dictionary, list or tuple - got '{}'".format(type(devices)))
+    hashes = list()
+    for device in devices:
+        hashes.append(device.hash)
+    hashes.sort()
+    return hashlib.sha1(''.join(hashes).encode()).hexdigest()
+
+
 class Client(metaclass=Singleton):
     __out_queue = Queue()
     __in_queue = Queue()
@@ -53,10 +78,13 @@ class Client(metaclass=Singleton):
 
     def __init__(self, device_manager, con_callbck=None, discon_callbck=None):
         if not device_manager:
-            raise RuntimeError("connector-client must be provided with a device manager")
-        if not issubclass(device_manager, DeviceManagerInterface):
-            raise TypeError("provided device manager must subclass DeviceManagerInterface")
-        __class__.__device_manager = device_manager
+            raise RuntimeError("a device manager must be provided")
+        if isclass(device_manager):
+            if _interfaceCheck(device_manager, DeviceManagerInterface):
+                __class__.__device_manager = device_manager()
+        else:
+            if _interfaceCheck(type(device_manager), DeviceManagerInterface):
+                __class__.__device_manager = device_manager
         self.__con_callbck = con_callbck
         self.__discon_callbck = discon_callbck
         self.__session_manager = SessionManager()
@@ -65,151 +93,6 @@ class Client(metaclass=Singleton):
         self.__router_thread = Thread(target=self.__router, name="Router")
         self.__router_thread.start()
         #self.__connect()
-
-
-    def __reconnect(self):
-        __class__.__ready = False
-        if self.__discon_callbck:
-            _callInThread(self.__discon_callbck)
-        reconnect = Thread(target=self.__connect, name='reconnect', args=(30, ))
-        logger.info("reconnecting in 30s")
-        reconnect.start()
-
-    '''   
-    def __connect(self, wait=None):
-        if wait:
-            time.sleep(wait)
-        websocket = Websocket(CONNECTOR_HOST, CONNECTOR_PORT, self.__reconnect)
-        logger.info('trying to connect to SEPL connector')
-        if _callAndWaitFor(websocket.connect):
-            logger.info("connected to SEPL connector")
-            logger.debug("starting handshake")
-            credentials = {
-                'user': CONNECTOR_USER,
-                'pw': CONNECTOR_PASSWORD,
-                'token': str(uuid())
-            }
-            logger.debug('sending credentials: {}'.format(credentials))
-            if _callAndWaitFor(websocket.send, json.dumps(credentials)):
-                answer = _callAndWaitFor(websocket.receive, timeout=10)
-                if answer:
-                    logger.debug('received answer: {}'.format(answer))
-                    status, token, message = _parsePackage(answer)
-                    if status == 'response' and token == credentials['token'] and message == 'ok':
-                        logger.info('handshake completed')
-                        _callAndWaitFor(websocket.ioStart, __class__.__in_queue, __class__.__out_queue)
-                        __class__.__ready = True
-                        if self.__listenAllDevices():
-                            logger.info('connector-client ready')
-                            if self.__con_callbck:
-                                _callInThread(self.__con_callbck)
-                            return True
-                        else:
-                            logger.error('could not register known devices')
-                    else:
-                        logger.error('handshake failed')
-                else:
-                    logger.error('handshake timed out')
-            else:
-                logger.error('could not initiate handshake')
-        else:
-            logger.error('could not connect')
-        _callAndWaitFor(websocket.shutdown)
-        return False
-    '''
-
-    def __connect(self, wait=None):
-        if wait:
-            time.sleep(wait)
-        credentials = json.dumps({
-            'user': CONNECTOR_USER,
-            'pw': CONNECTOR_PASSWORD,
-            'gid': CONNECTOR_GID,
-            'token': 'credentials'
-        })
-        websocket = Websocket(CONNECTOR_HOST, CONNECTOR_PORT, self.__reconnect)
-        logger.info('trying to connect to SEPL connector')
-        if _callAndWaitFor(websocket.connect):
-            logger.info("connected to SEPL connector")
-            logger.debug("starting handshake")
-            logger.debug('sending credentials: {}'.format(credentials))
-            if _callAndWaitFor(websocket.send, credentials):
-                initial_response = _callAndWaitFor(websocket.receive, timeout=10)
-                if initial_response:
-                    logger.debug('received initial message: {}'.format(initial_response))
-                    intitial_response = unmarshalMsg(initial_response)
-                    if intitial_response.status == 200 and getMangledAttr(intitial_response, 'token') == 'credentials':
-                        logger.debug('check if gateay ID needs to be synchronised')
-                        self.__synchroniseGid(intitial_response.payload.get('gid'))
-                        logger.info('handshake completed')
-                        _callAndWaitFor(websocket.ioStart, __class__.__in_queue, __class__.__out_queue)
-                        logger.info('checking if devices need to be synchronised')
-                        if self.__synchroniseDevices(intitial_response.payload.get('hash')):
-                            logger.info('synchronised devices')
-                            logger.info('connector-client ready')
-                            if self.__con_callbck:
-                                _callInThread(self.__con_callbck)
-                            return True
-                    else:
-                        logger.error('handshake failed - {} {}'.format(intitial_response.payload, intitial_response.status))
-                else:
-                    logger.error('handshake failed - timed out')
-            else:
-                logger.error('could not initiate handshake')
-        else:
-            logger.error('could not connect')
-        _callAndWaitFor(websocket.shutdown)
-        return False
-
-
-    def __synchroniseGid(self, remote_gid):
-        if not CONNECTOR_GID == remote_gid:
-            logger.debug('local and remote gateway ID differ: {} - {}'.format(CONNECTOR_GID, remote_gid))
-            global CONNECTOR_GID
-            CONNECTOR_GID = remote_gid
-            writeConf(section='CONNECTOR', parameter='gid', value=remote_gid)
-            logger.info("set gateway ID: '{}'".format(remote_gid))
-        else:
-            logger.debug('local and remote gateway ID match')
-
-
-    def __hashDevices(self, devices):
-        if type(devices) not in (dict, list, tuple):
-            raise TypeError("please provide devices via dictionary, list or tuple - got '{}'".format(type(devices)))
-        hashes = list()
-        for device in devices:
-            hashes.append(device.hash)
-        hashes.sort()
-        return hashlib.sha1(''.join(hashes).encode()).hexdigest()
-
-
-    def __synchroniseDevices(self, remote_hash):
-        dm = __class__.__device_manager()
-        devices = dm.devices
-        local_hash = self.__hashDevices(devices)
-        logger.debug('calculated local hash: {}'.format(local_hash))
-        if not local_hash == remote_hash:
-            logger.debug('local and remote hash differ: {} - {}'.format(local_hash, remote_hash))
-            clr_msg = Message(handlers['clear_handler'])
-            response = __class__.__send(clr_msg)
-            response = unmarshalMsg(response)
-            if response.status == 200:
-                for device in devices:
-                    if not __class__.register(device):
-                        logger.error("synchronisation failed - device could not be synchronised")
-                        return False
-                commit_msg = Message(handlers['commit_handler'])
-                response = __class__.__send(commit_msg)
-                if response.status == 200:
-                    return True
-                logger.error("synchronisation failed - could not commit changes")
-                return False
-            else:
-                logger.error("synchronisation could not be initiated - '{} {}'".format(response.payload, response.status))
-                return False
-        else:
-            logger.debug('local and remote hash match')
-        return True
 
 
     def __callbackHandler(self):
@@ -227,6 +110,85 @@ class Client(metaclass=Singleton):
                     __class__.__client_queue.put(msg_obj)
                 else:
                     SessionManager.raiseEvent(msg_obj, getMangledAttr(msg_obj, 'token'))
+
+
+    def __connect(self, wait=None) -> bool:
+        if wait:
+            time.sleep(wait)
+        credentials = json.dumps({
+            'user': CONNECTOR_USER,
+            'pw': CONNECTOR_PASSWORD,
+            'gid': CONNECTOR_GID,
+            'token': 'credentials'
+        })
+        websocket = Websocket(CONNECTOR_HOST, CONNECTOR_PORT, self.__reconnect)
+        logger.info('trying to connect to SEPL connector')
+        if _callAndWaitFor(websocket.connect):
+            logger.info("connected to SEPL connector")
+            logger.debug("starting handshake")
+            logger.debug('sending credentials: {}'.format(credentials))
+            if _callAndWaitFor(websocket.send, credentials):
+                initial_response = _callAndWaitFor(websocket.receive, timeout=10)
+                if initial_response:
+                    logger.debug('received initial response: {}'.format(initial_response))
+                    initial_response = unmarshalMsg(initial_response)
+                    if initial_response and initial_response.status == 200 and getMangledAttr(initial_response, 'token') == 'credentials':
+                        logger.debug('check if gateay ID needs to be synchronised')
+                        _synchroniseGid(initial_response.payload.get('gid'))
+                        logger.info('handshake completed')
+                        _callAndWaitFor(websocket.ioStart, __class__.__in_queue, __class__.__out_queue)
+                        logger.info('checking if devices need to be synchronised')
+                        if self.__synchroniseDevices(initial_response.payload.get('hash')):
+                            logger.info('synchronised devices')
+                            logger.info('connector-client ready')
+                            if self.__con_callbck:
+                                _callInThread(self.__con_callbck)
+                            return True
+                    else:
+                        logger.error('handshake failed - {} {}'.format(initial_response.payload, initial_response.status))
+                else:
+                    logger.error('handshake failed - timed out')
+            else:
+                logger.error('could not initiate handshake')
+        else:
+            logger.error('could not connect')
+        _callAndWaitFor(websocket.shutdown)
+        return False
+
+
+    def __reconnect(self):
+        __class__.__ready = False
+        if self.__discon_callbck:
+            _callInThread(self.__discon_callbck)
+        reconnect = Thread(target=self.__connect, name='reconnect', args=(30, ))
+        logger.info("reconnecting in 30s")
+        reconnect.start()
+
+
+    def __synchroniseDevices(self, remote_hash) -> bool:
+        devices = __class__.__device_manager.devices
+        local_hash = _hashDevices(devices)
+        logger.debug('calculated local hash: {}'.format(local_hash))
+        if not local_hash == remote_hash:
+            logger.debug('local and remote hash differ: {} - {}'.format(local_hash, remote_hash))
+            clr_msg = Message(handlers['clear_handler'])
+            response = __class__.__send(clr_msg)
+            response = unmarshalMsg(response)
+            if response and response.status == 200:
+                for device in devices:
+                    if not __class__.__put(device):
+                        logger.error("synchronisation failed - device '{}' could not be synchronised".format(device.name))
+                        return False
+                if __class__.__commit(local_hash):
+                    return True
+                logger.error("synchronisation failed - could not commit changes")
+                return False
+            else:
+                logger.error("synchronisation could not be initiated - '{} {}'".format(response.payload, response.status))
+                return False
+        else:
+            logger.debug('local and remote hash match')
+        return True
 
 
     @staticmethod
@@ -248,6 +210,34 @@ class Client(metaclass=Singleton):
             SessionManager.new(msg_obj, token, timeout, callback)
             __class__.__out_queue.put(msg_str)
             logger.debug('send: {}'.format(msg_str))
+
+
+    @staticmethod
+    def __put(device) -> bool:
+        put_msg = Message(handlers['put_handler'])
+        put_msg.payload = {
+            'uri': device.id,
+            'name': device.name,
+            'tags': device.tags
+        }
+        response = __class__.__send(put_msg)
+        if response.status == 200:
+            logger.debug("put device '{}'".format(device.id))
+            return True
+        logger.debug("put device '{}' failed".format(device.id))
+        return False
+
+
+    @staticmethod
+    def __commit(local_hash) -> bool:
+        commit_msg = Message(handlers['commit_handler'])
+        commit_msg.payload = local_hash
+        response = __class__.__send(commit_msg)
+        if response.status == 200:
+            logger.debug("commit '{}'".format(local_hash))
+            return True
+        logger.debug("commit '{}' failed".format(local_hash))
+        return False
 
 
     #--------- User methods ---------#
@@ -281,7 +271,7 @@ class Client(metaclass=Singleton):
 
 
     @staticmethod
-    def response(msg_obj, payload, **kwargs):
+    def response(msg_obj, payload, **kwargs) -> Message:
         if type(msg_obj) is not Message:
             raise TypeError("msg_obj must be Message but got '{}'".format(type(msg_obj)))
         # if type(payload) is not str:
@@ -300,44 +290,49 @@ class Client(metaclass=Singleton):
     def register(device) -> bool:
         if type(device) is not Device:
             raise TypeError("register takes a 'Device' object but got '{}'".format(type(device)))
-        dm = __class__.__device_manager()
-        dm.add(device)
-        put_msg = Message(handlers['put_handler'])
-        put_msg.payload = {
-            'uri': device.id,
-            'name': device.name,
-            'tags': device.tags
-        }
-        response = __class__.__send(put_msg)
-        if response.status == 200:
-            logger.info("registered device '{}'".format(device.name))
-            return True
+        __class__.__device_manager.add(device)
+        local_hash = _hashDevices(__class__.__device_manager.devices)
+        if __class__.__put(device):
+            if __class__.__commit(local_hash):
+                logger.info("registered device '{}'".format(device.name))
+                return True
         logger.warning("could not register device '{}'".format(device.name))
         return False
 
 
+    @staticmethod
+    def update(device) -> bool:
+        if type(device) is not Device:
+            raise TypeError("update takes a 'Device' object but got '{}'".format(type(device)))
+        __class__.__device_manager.update(device)
+        local_hash = _hashDevices(__class__.__device_manager.devices)
+        if __class__.__put(device):
+            if __class__.__commit(local_hash):
+                logger.info("updated device '{}'".format(device.name))
+                return True
+        logger.warning("could not update device '{}'".format(device.name))
+        return False
 
 
-
+    @staticmethod
+    def remove(device) -> bool:
+        if type(device) is Device:
+            d_id = device.id
+        elif type(device) is str:
+            d_id = device
+        else:
+            raise TypeError("device must be string or Device but got '{}'".format(type(device)))
+        __class__.__device_manager.remove(d_id)
+        mute_msg = Message(handlers['mute_handler'])
+        response = __class__.__send(mute_msg)
+        response = unmarshalMsg(response)
+        if response and response.status == 200:
+            logger.info("removed device '{}'".format(device.name))
+            return True
+        logger.warning("could not remove device '{}'".format(device.name))
+        return False
 
 
     @staticmethod
     def receive() -> Message:
         return __class__.__client_queue.get()
-
-
-    '''
-    @staticmethod
-    def update(device) -> bool:
-        if type(device) is not Device:
-            raise TypeError("update takes a 'Device' object but got '{}'".format(type(device)))
-        dm = __class__.__device_manager()
-        dm.update(device)
-        response = __class__.send(_UpdateName(device))
-        response2 = __class__.send(_UpdateTags(device))
-        if type(response) is Response and type(response2) is Response:
-            logger.info("updated device '{}'".format(device.name))
-            return True
-        logger.warning("could not update device '{}'".format(device.name))
-        return False
-    '''
