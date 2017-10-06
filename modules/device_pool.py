da01101e93931e7f773807360cafd518aa7f2891
y-du <42994541+y-du@users.noreply.github.com>
2017-10-06 15:21:34 +0200

    implemented new device check

--- modules/device_pool.py
+++ modules/device_pool.py
@@ -5,7 +5,7 @@ try:
     from modules.logger import root_logger
     from modules.singleton import SimpleSingleton
     from connector.dm_interface import DeviceManagerInterface
-    from connector.device import Device
+    from connector.device import Device, _isDevice
 except ImportError as ex:
     exit("{} - {}".format(__name__, ex.msg))
 
@@ -16,30 +16,30 @@ class DevicePool(DeviceManagerInterface, SimpleSingleton):
     __pool = dict()
 
     def add(self, device):
-        if type(device) is not Device:
-            raise TypeError("a Device object must be provided but got a '{}'".format(type(device)))
+        if not _isDevice(device):
+            raise TypeError("device must be Device or subclass of Device but got '{}'".format(type(device)))
         if device.id not in __class__.__pool:
             __class__.__pool[device.id] = device
         else:
             logger.warning("device '{}' already in pool".format(device.id))
 
     def update(self, device):
-        if type(device) is not Device:
-            raise TypeError("a Device object must be provided but got a '{}'".format(type(device)))
+        if not _isDevice(device):
+            raise TypeError("device must be Device or subclass of Device but got '{}'".format(type(device)))
         if device.id in __class__.__pool:
             __class__.__pool[device.id] = device
         else:
             logger.error("can't update device '{}' please add it first".format(device.id))
 
-    def remove(self, d_id):
-        if type(d_id) is Device:
-            d_id = d_id.id
-        elif type(d_id) is not str:
-            raise TypeError("a string or a Device object must be provided but got a '{}'".format(type(d_id)))
+    def remove(self, device):
+        if _isDevice(device):
+            device = device.id
+        elif type(device) is not str:
+            raise TypeError("device must be Device, subclass of Device or string (if ID only) but got '{}'".format(type(device)))
         try:
-            del __class__.__pool[d_id]
+            del __class__.__pool[device]
         except KeyError:
-            logger.error("device '{}' does not exist in device pool".format(d_id))
+            logger.error("device '{}' does not exist in device pool".format(device))
 
     def get(self, id_str) -> Device:
         if type(id_str) is not str:
