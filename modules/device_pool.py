91da4fe8ebab6f04fdaf3d14e4e2ac56c25827a2
y-du <42994541+y-du@users.noreply.github.com>
2017-10-09 10:15:00 +0200

    changed to static methods

--- modules/device_pool.py
+++ modules/device_pool.py
@@ -14,7 +14,8 @@ logger = root_logger.getChild(__name__)
 class DevicePool(DeviceManagerInterface, SimpleSingleton):
     __pool = dict()
 
-    def add(self, device):
+    @staticmethod
+    def add(device):
         if not _isDevice(device):
             raise TypeError("device must be Device or subclass of Device but got '{}'".format(type(device)))
         if device.id not in __class__.__pool:
@@ -22,7 +23,8 @@ class DevicePool(DeviceManagerInterface, SimpleSingleton):
         else:
             logger.warning("device '{}' already in pool".format(device.id))
 
-    def update(self, device):
+    @staticmethod
+    def update(device):
         if not _isDevice(device):
             raise TypeError("device must be Device or subclass of Device but got '{}'".format(type(device)))
         if device.id in __class__.__pool:
@@ -30,7 +32,8 @@ class DevicePool(DeviceManagerInterface, SimpleSingleton):
         else:
             logger.error("can't update device '{}' please add it first".format(device.id))
 
-    def remove(self, device):
+    @staticmethod
+    def remove(device):
         if _isDevice(device):
             device = device.id
         elif type(device) is not str:
@@ -40,11 +43,12 @@ class DevicePool(DeviceManagerInterface, SimpleSingleton):
         except KeyError:
             logger.error("device '{}' does not exist in device pool".format(device))
 
-    def get(self, id_str) -> Device:
+    @staticmethod
+    def get(id_str) -> Device:
         if type(id_str) is not str:
             raise TypeError("id must be a string but got '{}'".format(type(id_str)))
         return __class__.__pool.get(id_str)
 
-    @property
-    def devices(self) -> dict:
+    @staticmethod
+    def devices() -> dict:
         return __class__.__pool.copy()
