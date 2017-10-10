bc694e8450c521060e3c06078639b1aa4a87ab04
y-du <42994541+y-du@users.noreply.github.com>
2017-10-10 09:57:31 +0200

    documentation

--- connector/message.py
+++ connector/message.py
@@ -12,6 +12,7 @@ from uuid import uuid4 as uuid
 logger = root_logger.getChild(__name__)
 
 
+# platform handlers map
 handlers = {
     'put_handler': 'put',
     'mute_handler': 'mute',
@@ -23,7 +24,15 @@ handlers = {
 }
 
 class Message:
+    """
+    Class mapping the message (or envelope) structure used by the platform.
+    """
     def __init__(self, handler=str()):
+        """
+        Create a Message object. Attributes must be set after instantiation.
+        Users can only access 'status', 'content_type', 'payload'.
+        :param handler: Optional handler. Use platform handlers map above.
+        """
         self.__status = int()
         self.__handler = handler
         self.__token = str(uuid())
@@ -50,18 +59,40 @@ class Message:
 
 
 def getMangledAttr(obj, attr):
+    """
+    Read mangled attribute.
+    :param obj: Object with mangled attributes.
+    :param attr: Name of mangled attribute.
+    :return: value of mangled attribute.
+    """
     return getattr(obj, '_{}__{}'.format(obj.__class__.__name__, attr))
 
 
 def setMangledAttr(obj, attr, arg):
+    """
+    Write to mangled attribute.
+    :param obj: Object with mangled attributes.
+    :param attr: Name of mangled attribute.
+    :param arg: value to be written.
+    """
     setattr(obj, '_{}__{}'.format(obj.__class__.__name__, attr), arg)
 
 
 def marshalMsg(msg_obj: Message) -> str:
+    """
+    Marshal a Message object to JSON.
+    :param msg_obj: Message object.
+    :return: String.
+    """
     return json.dumps({key.replace('_{}__'.format(msg_obj.__class__.__name__), ''): msg_obj.__dict__[key] for key in msg_obj.__dict__})
 
 
 def unmarshalMsg(msg_str: str) -> Message:
+    """
+    Unmarshal JSON message.
+    :param msg_str: JSON message as string.
+    :return: Message object.
+    """
     try:
         msg = json.loads(msg_str)
         msg_obj = Message()
