6d7d8635c8f15925f7a1e3f25745b81fafc0e529
y-du <42994541+y-du@users.noreply.github.com>
2017-10-24 11:36:50 +0200

    refactor and added documentation

--- connector/client.py
+++ connector/client.py
@@ -23,11 +23,24 @@ logger.info(20 * '*' + ' Starting SEPL connector-client ' + 20 * '*')
 
 
 def _callback(event, message=None):
+    """
+    Adds a message to an Event and sets it.
+    :param event: Event object.
+    :param message: Event message.
+    """
     event.message = message
     event.set()
 
 
 def _callAndWaitFor(function, *args, timeout=None, **kwargs):
+    """
+    Call a method containing an Event and wait for completion.
+    :param function: Method with ability to call a callback to set a event.
+    :param args: User arguments.
+    :param timeout: Time in sec to wait for method to complete.
+    :param kwargs: User arguments.
+    :return: Event message.
+    """
     event = Event()
     event.message = None
     function(functools.partial(_callback, event), *args, **kwargs)
@@ -36,12 +49,23 @@ def _callAndWaitFor(function, *args, timeout=None, **kwargs):
 
 
 def _interfaceCheck(cls, interface):
+    """
+    Checks if a class subclasses another class.
+    Raises TypeError on mismatch.
+    :param cls: Class to check.
+    :param interface: Class that should be subclassed.
+    :return: Boolean.
+    """
     if issubclass(cls, interface):
         return True
     raise TypeError("provided class '{}' must be a subclass of '{}'".format(cls, interface))
 
 
 def _synchroniseGid(remote_gid):
+    """
+    Checks if local and remote gateway ID differ and sets new ID.
+    :param remote_gid: Remote gateway ID as string.
+    """
     global CONNECTOR_GID
     if not CONNECTOR_GID == remote_gid:
         logger.debug('local and remote gateway ID differ: {} - {}'.format(CONNECTOR_GID, remote_gid))
@@ -53,6 +77,11 @@ def _synchroniseGid(remote_gid):
 
 
 def _hashDevices(devices) -> str:
+    """
+    Hashes provided devices with SHA1.
+    :param devices: List, tuple or dict (id:device) of local devices.
+    :return: Hash as string.
+    """
     if type(devices) not in (dict, list, tuple):
         raise TypeError("please provide devices via dictionary, list or tuple - got '{}'".format(type(devices)))
     if type(devices) is dict:
@@ -65,6 +94,12 @@ def _hashDevices(devices) -> str:
 
 
 class Client(metaclass=Singleton):
+    """
+    Connector-client for integrating personal IoT projects / devices with the SEPL platform.
+    To avoid multiple instantiations the connector-client implements the singleton pattern.
+    The client API uses static methods, thus allowing calls directly from the class or an object.
+    Threading is managed internally, wrapping the connector-client in a thread is not necessary.
+    """
     __out_queue = Queue()
     __in_queue = Queue()
     __client_queue = Queue()
@@ -73,6 +108,13 @@ class Client(metaclass=Singleton):
 
 
     def __init__(self, device_manager, con_callbck=None, discon_callbck=None):
+        """
+        Creates a Client instance, checks for a device manager, starts session manager, callback handler and router.
+        Connects to the SEPL platform on first run and returns control to caller.
+        :param device_manager: Required (class or object), must implement DeviceManagerInterface.
+        :param con_callbck: Method to be called after successful connection to SEPL platform.
+        :param discon_callbck: Method to be called upon disconnect event.
+        """
         if not device_manager:
             raise RuntimeError("a device manager must be provided")
         elif isclass(device_manager):
@@ -92,12 +134,18 @@ class Client(metaclass=Singleton):
 
 
     def __callbackHandler(self):
+        """
+        Execute callbacks from the session manager.
+        """
         while True:
             callback, msg_obj = SessionManager.callback_queue.get()
             callback(msg_obj)
 
 
     def __router(self):
+        """
+        Routes tasks / commands to the user and raises events for remaining messages.
+        """
         while True:
             msg_str = __class__.__in_queue.get()
             msg_obj = unmarshalMsg(msg_str)
@@ -109,6 +157,12 @@ class Client(metaclass=Singleton):
 
 
     def __connect(self, wait=None) -> bool:
+        """
+        Connects to the SEPL platform.
+        Performs handshake, starts device synchronisation and sets connector-client to ready on successful connection.
+        :param wait: Time in sec to wait until a connection is attempted.
+        :return: Boolean.
+        """
         if wait:
             if self.__discon_callbck:
                 self.__discon_callbck()
@@ -156,6 +210,9 @@ class Client(metaclass=Singleton):
 
 
     def __reconnect(self):
+        """
+        Calls __connect() wrapped in a thread on a reconnect event.
+        """
         __class__.__ready = False
         reconnect = Thread(target=self.__connect, name='reconnect', args=(30, ))
         logger.info("reconnecting in 30s")
@@ -163,6 +220,11 @@ class Client(metaclass=Singleton):
 
 
     def __synchroniseDevices(self, remote_hash) -> bool:
+        """
+        Synchronises local devices during connection phase.
+        :param remote_hash: Hash stored on the SEPL platform.
+        :return: Boolean.
+        """
         devices = __class__.__device_manager.devices()
         if type(devices) is dict:
             devices = list(devices.values())
@@ -193,6 +255,14 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def __send(msg_obj, timeout=10, callback=None, block=True) -> Message:
+        """
+        Send messages to the SEPL platform.
+        :param msg_obj: Message object.
+        :param timeout: Timeout in sec.
+        :param callback: Method to be called on response or timeout.
+        :param block: Boolean, set to False for asynchronous behavior.
+        :return: Message object.
+        """
         msg_str = marshalMsg(msg_obj)
         token = getMangledAttr(msg_obj, 'token')
         if block:
@@ -208,7 +278,31 @@ class Client(metaclass=Singleton):
 
 
     @staticmethod
+    def __commit(local_hash, **kwargs) -> bool:
+        """
+        Commits changes on the SEPL platform.
+        :param local_hash: Hash calculated from devices provided via a device manager.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Boolean.
+        """
+        commit_msg = Message(handlers['commit_handler'])
+        commit_msg.payload = local_hash
+        response = __class__.__send(commit_msg, **kwargs)
+        if response.status == 200:
+            logger.debug("commit '{}'".format(local_hash))
+            return True
+        logger.debug("commit '{}' failed".format(local_hash))
+        return False
+
+
+    @staticmethod
     def __put(device, **kwargs) -> bool:
+        """
+        PUT a device to the SEPL platform. Commit required after call.
+        :param device: Device (or subclass of Device) object.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return:
+        """
         put_msg = Message(handlers['put_handler'])
         put_msg.payload = {
             'uri': device.id,
@@ -225,19 +319,13 @@ class Client(metaclass=Singleton):
 
 
     @staticmethod
-    def __commit(local_hash, **kwargs) -> bool:
-        commit_msg = Message(handlers['commit_handler'])
-        commit_msg.payload = local_hash
-        response = __class__.__send(commit_msg, **kwargs)
-        if response.status == 200:
-            logger.debug("commit '{}'".format(local_hash))
-            return True
-        logger.debug("commit '{}' failed".format(local_hash))
-        return False
-
-
-    @staticmethod
     def __disconnect(device_id, **kwargs) -> bool:
+        """
+        Disconnects a device from the SEPL platform. Commit required after call.
+        :param device_id: Device ID as string.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Boolean.
+        """
         discon_msg = Message(handlers['disconnect_handler'])
         discon_msg.payload = device_id
         response = __class__.__send(discon_msg, **kwargs)
@@ -250,6 +338,12 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def __delete(device_id, **kwargs) -> bool:
+        """
+        Deletes a device from the SEPL platform. Commit required after call.
+        :param device_id: Device ID as string.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Boolean.
+        """
         del_msg = Message(handlers['delete_handler'])
         del_msg.payload = device_id
         response = __class__.__send(del_msg, **kwargs)
@@ -265,6 +359,14 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def event(device, service, payload, **kwargs) -> Message:
+        """
+        User method for pushing events to the SEPL platform.
+        :param device: Device ID or a Device (or subclass of Device) object.
+        :param service: SEPL service.
+        :param payload: Event payload as string.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Message object.
+        """
         if _isDevice(device):
             d_id = device.id
         elif type(device) is str:
@@ -291,6 +393,12 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def response(msg_obj, payload, **kwargs):
+        """
+        User method for responding to a task / command from the SEPL platform.
+        :param msg_obj: Original Message object from a task / command.
+        :param payload: Data concerning the completion of a task / command.
+        :param kwargs: timeout=10, callback=None, block=True.
+        """
         if type(msg_obj) is not Message:
             raise TypeError("msg_obj must be Message but got '{}'".format(type(msg_obj)))
         setMangledAttr(msg_obj, 'handler', handlers['response_handler'])
@@ -308,6 +416,12 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def add(device, **kwargs) -> bool:
+        """
+        User method for adding devices.
+        :param device: Device (or subclass of Device) object.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Boolean.
+        """
         if not _isDevice(device):
             raise TypeError("device must be Device or subclass of Device but got '{}'".format(type(device)))
         __class__.__device_manager.add(device)
@@ -323,6 +437,12 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def update(device, **kwargs) -> bool:
+        """
+        User method for updating devices.
+        :param device: Device (or subclass of Device) object.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Boolean.
+        """
         if not _isDevice(device):
             raise TypeError("device must be Device or subclass of Device but got '{}'".format(type(device)))
         __class__.__device_manager.update(device)
@@ -338,6 +458,12 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def disconnect(device, **kwargs) -> bool:
+        """
+        User method for disconnecting devices.
+        :param device: Device ID or a Device (or subclass of Device) object.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Boolean.
+        """
         if _isDevice(device):
             device = device.id
         elif type(device) is not str:
@@ -355,6 +481,12 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def delete(device, **kwargs) -> bool:
+        """
+        User method for deleting devices.
+        :param device: Device ID or a Device (or subclass of Device) object.
+        :param kwargs: timeout=10, callback=None, block=True.
+        :return: Boolean.
+        """
         if _isDevice(device):
             device = device.id
         elif type(device) is not str:
@@ -373,4 +505,8 @@ class Client(metaclass=Singleton):
 
     @staticmethod
     def receive() -> Message:
+        """
+        User method for receiving tasks / commands from the SEPL platform.
+        :return: Message object.
+        """
         return __class__.__client_queue.get()
