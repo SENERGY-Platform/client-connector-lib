0f3b16e6a88a7b45be4736ed7bf55f112870b0f1
y-du <42994541+y-du@users.noreply.github.com>
2019-02-15 15:08:18 +0100

    import from new path for legacy support

--- connector_lib/modules/singleton.py
+++ connector_lib/modules/singleton.py
@@ -14,23 +14,4 @@
    limitations under the License.
 """
 
-class Singleton(type):
-    """
-    Subclass this class for singleton behavior
-    """
-    _instances = dict()
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
-class SimpleSingleton:
-    """
-    Classes inheriting from abstract classes subclass this class for singleton behavior
-    """
-    _instance = None
-    def __new__(cls, *args, **kwargs):
-        if not isinstance(cls._instance, cls):
-            cls._instance = object.__new__(cls, *args, **kwargs)
-        return cls._instance
+from ..client.singleton import Singleton, SimpleSingleton
