751c9d7e5252947457bcf90b9d81bc3fd145ff16
y-du <42994541+y-du@users.noreply.github.com>
2019-02-22 00:56:25 +0100

    remove imports

--- connector_lib/client/__init__.py
+++ connector_lib/client/__init__.py
@@ -13,6 +13,3 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
-
-from .client import Client
-from .logger import connector_lib_log_handler
