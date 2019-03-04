433e8c907001c01cb0e943a006e6a850d5fa467c
y-du <42994541+y-du@users.noreply.github.com>
2019-03-04 16:37:12 +0100

    import subpackages 'http', 'mqtt' and 'websocket'

--- cc_lib/connector/protocol/__init__.py
+++ cc_lib/connector/protocol/__init__.py
@@ -13,3 +13,5 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+
+import cc_lib.connector.protocol.http, cc_lib.connector.protocol.mqtt, cc_lib.connector.protocol.websocket
