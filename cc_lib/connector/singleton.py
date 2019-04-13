37f31e24942c06c7385387e89c801952a5f76d18
y-du <42994541+y-du@users.noreply.github.com>
2019-04-13 11:43:22 +0200

    remove old class

--- cc_lib/connector/singleton.py
+++ cc_lib/connector/singleton.py
@@ -23,14 +23,3 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
-
-
-class SimpleSingleton:
-    """
-    Classes inheriting from abstract classes subclass this class for singleton behavior
-    """
-    _instance = None
-    def __new__(cls, *args, **kwargs):
-        if not isinstance(cls._instance, cls):
-            cls._instance = object.__new__(cls, *args, **kwargs)
-        return cls._instance
