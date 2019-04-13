e372e237ee84ff0bd3fd73aad134742973c5a884
y-du <42994541+y-du@users.noreply.github.com>
2019-04-13 10:23:30 +0200

    remove Envelope class, make metadata optional

--- cc_lib/connector/message/message.py
+++ cc_lib/connector/message/message.py
@@ -15,16 +15,15 @@
 """
 
 
-__all__ = ("Message", "Envelope")
+__all__ = ("Message", )
 
 
 from typing import Optional, Type
-from uuid import uuid4 as uuid
 
 
 class Message:
-    def __init__(self, data: str, metadata: str):
-        self.metadata = metadata
+    def __init__(self, data: str, metadata: Optional[str] = None):
+        self.metadata = metadata or str()
         self.data = data
 
     @property
@@ -63,61 +62,5 @@ class Message:
         attributes = [
             ('metadata', self.metadata),
             ('data', self.data)
-        ]
-        return "{}({})".format(__class__.__name__, ", ".join(["=".join([key, str(value)]) for key, value in attributes]))
-
-
-class Envelope:
-    def __init__(self, device_id: str, service_uri: str, message: Message, corr_id: Optional[str] = None):
-        __class__.__checkType(device_id, str)
-        __class__.__checkType(service_uri, str)
-        if corr_id:
-            __class__.__checkType(corr_id, str)
-        self.__correlation_id = corr_id or uuid().hex
-        self.__device_id = device_id
-        self.__service_uri = service_uri
-        self.message = message
-
-    @property
-    def correlation_id(self) -> str:
-        return self.__correlation_id
-
-    @property
-    def device_id(self) -> str:
-        return self.__device_id
-
-    @property
-    def service_uri(self) -> str:
-        return self.__service_uri
-
-    @property
-    def message(self) -> Message:
-        return self.__message
-
-    @message.setter
-    def message(self, arg):
-        __class__.__checkType(arg, Message)
-        self.__message = arg
-
-    @staticmethod
-    def __checkType(arg: object, typ: Type) -> None:
-        """
-        Check if arg is the correct type. Raise exception if not.
-        :param: arg: object to check
-        :param: typ: type
-        """
-        if not type(arg) is typ:
-            raise TypeError(type(arg))
-
-    def __repr__(self):
-        """
-        Provide a string representation.
-        :return: String.
-        """
-        attributes = [
-            ('correlation_id', self.correlation_id),
-            ('device_id', self.device_id),
-            ('service_uri', self.service_uri),
-            ('message', self.message)
         ]
         return "{}({})".format(__class__.__name__, ", ".join(["=".join([key, str(value)]) for key, value in attributes]))
