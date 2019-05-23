6ae928d49073fd5c5151fb1e3ffca09ed913c5eb
y-du <42994541+y-du@users.noreply.github.com>
2019-05-23 21:37:31 +0200

    move service decorator function to own file

--- cc_lib/device/device.py
+++ cc_lib/device/device.py
@@ -14,43 +14,10 @@
    limitations under the License.
 """
 
-__all__ = ('Device', 'service')
+__all__ = ('Device', )
 
 from collections import OrderedDict
-from typing import Type, Callable, Any
 from hashlib import sha1
-
-
-def service(input=None, output=None):
-    if any((type(input) is type, type(output) is type)):
-        def wrap(func: Callable[[Any], Any]):
-            def wrap_call(*args, **kwargs):
-                return func(*args, **kwargs)
-            setattr(wrap_call, "__service__", True)
-            if input:
-                setattr(
-                    wrap_call,
-                    "__input__",
-                    {key: value for key, value in input.__dict__.items() if not key.startswith('_')}
-                )
-            else:
-                setattr(wrap_call, "__input__", None)
-            if output:
-                setattr(
-                    wrap_call,
-                    "__output__",
-                    {key: value for key, value in output.__dict__.items() if not key.startswith('_')}
-                )
-            else:
-                setattr(wrap_call, "__output__", None)
-            return wrap_call
-    else:
-        def wrap(*args, **kwargs):
-            return input(*args, **kwargs)
-        setattr(wrap, "__service__", True)
-        setattr(wrap, "__input__", None)
-        setattr(wrap, "__output__", None)
-    return wrap
 
 
 class Device:
