ece815a553a6fb4283813fdcdc6748042e7eb487
y-du <42994541+y-du@users.noreply.github.com>
2019-05-27 13:48:56 +0200

    move service class to own sub package

--- cc_lib/types/service/__init__.py
+++ cc_lib/types/service/__init__.py
@@ -14,9 +14,8 @@
    limitations under the License.
 """
 
-from ._device import *
-import cc_lib.types.service
+from ._service import *
 
 __all__ = (
-    _device.__all__,
+    _service.__all__,
 )
