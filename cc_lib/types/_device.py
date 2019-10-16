f2e1ca4efe94a65e637d96337171bf41c28d6ced
y-du <42994541+y-du@users.noreply.github.com>
2019-10-16 13:36:37 +0200

    remove tag functionality from devices

--- cc_lib/types/_device.py
+++ cc_lib/types/_device.py
@@ -69,59 +69,6 @@ class Device:
         validateInstance(arg, str)
         self.__name = arg
 
-    @property
-    def tags(self) -> list:
-        """
-        Combine tag IDs and tags as key:value pairs.
-        :return: List.
-        """
-        return ['{}:{}'.format(key, value) for key, value in self.__tags.items()]
-
-    def addTag(self, tag_id: str, tag: str) -> None:
-        """
-        Add a tag to a device.
-        :param: tag_id: ID identifying the tag.
-        :param: tag: Word or combination of Words.
-        :return: None.
-        """
-        validateInstance(tag_id, str)
-        validateInstance(tag, str)
-        if tag_id in self.__tags.keys():
-            raise KeyError("tag id '{}' already in use".format(tag_id))
-        if ':' in tag_id or ';' in tag_id:
-            raise ValueError("tag id may not contain ':' or ';'")
-        if ':' in tag or ';' in tag:
-            raise ValueError("tag may not contain ':' or ';'")
-        self.__tags[tag_id] = tag
-
-    def changeTag(self, tag_id: str, tag: str) -> None:
-        """
-        Change existing tag.
-        :param: tag_id: ID identifying the tag.
-        :param: tag: Word or combination of Words.
-        :return: None.
-        """
-        validateInstance(tag_id, str)
-        validateInstance(tag, str)
-        if ':' in tag or ';' in tag:
-            raise ValueError("tag may not contain ':' or ';'")
-        if tag_id in self.__tags:
-            self.__tags[tag_id] = tag
-        else:
-            raise KeyError("tag id '{}' does not exist".format(tag_id))
-
-    def removeTag(self, tag_id: str) -> None:
-        """
-        Remove existing tag.
-        :param: tag_id: ID identifying the tag.
-        :return: Boolean
-        """
-        validateInstance(tag_id, str)
-        try:
-            del self.__tags[tag_id]
-        except KeyError:
-            raise KeyError("tag id '{}' does not exist".format(tag_id))
-
     def getService(self, service: str) -> Service:
         try:
             return self.__class__.services[service]
