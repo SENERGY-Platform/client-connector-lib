88574bfadc462bba9fa29b8ba40a189b8aac1aa0
y-du <42994541+y-du@users.noreply.github.com>
2019-12-04 11:46:09 +0100

    streamline imports

--- cc_lib/client/_asynchron/future.py
+++ cc_lib/client/_asynchron/future.py
@@ -17,7 +17,7 @@
 __all__ = ('Future', )
 
 
-from typing import Callable, Any, Optional
+import typing
 
 
 class FutureNotDoneError(Exception):
@@ -34,7 +34,7 @@ class Future:
     def __init__(self, worker):
         self.__worker = worker
 
-    def result(self) -> Any:
+    def result(self) -> typing.Any:
         if not self.__worker.done:
             raise FutureNotDoneError
         if self.__worker.exception:
@@ -47,10 +47,10 @@ class Future:
     def running(self) -> bool:
         return not self.__worker.done
 
-    def wait(self, timeout: Optional[float] = None) -> None:
+    def wait(self, timeout: typing.Optional[float] = None) -> None:
         self.__worker.join(timeout)
 
-    # def addDoneCallback(self, func: Callable[[], None]) -> None:
+    # def addDoneCallback(self, func: typing.Callable[[], None]) -> None:
     #     self.__worker.callback = func
 
     @property
