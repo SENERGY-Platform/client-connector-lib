3752c3cdf8fbf65f92c565a4d82f4746c804e1fb
y-du <42994541+y-du@users.noreply.github.com>
2019-12-04 12:25:09 +0100

    use validateInstance function from utils

--- cc_lib/client/message/_message.py
+++ cc_lib/client/message/_message.py
@@ -18,6 +18,7 @@
 __all__ = ("Message", )
 
 
+from ..._util import validateInstance
 import typing
 
 
@@ -35,7 +36,7 @@ class Message:
 
     @metadata.setter
     def metadata(self, arg: str):
-        __class__.__checkType(arg, str)
+        validateInstance(arg, str)
         self.__metadata = arg
 
     @property
@@ -44,18 +45,8 @@ class Message:
 
     @data.setter
     def data(self, arg: str):
-        __class__.__checkType(arg, str)
+        validateInstance(arg, str)
         self.__data = arg
-
-    @staticmethod
-    def __checkType(arg: object, typ: Type) -> None:
-        """
-        Check if arg is the correct type. Raise exception if not.
-        :param: arg: object to check
-        :param: typ: type
-        """
-        if not type(arg) is typ:
-            raise TypeError(type(arg))
 
     def __iter__(self):
         items = (('metadata', self.metadata), ('data', self.data))
